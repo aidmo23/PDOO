543fdce7d8781f0128365dab296cc62dcce67917
aiman <aiman@DESKTOP-GA7NEV4>
2024-10-10 10:35:24 +0200

    Creación de Trigo, atributos de clase e instancia. Closes #2

---
+++ MyHayDay/src/myhayday/Trigo.java
@@ -0,0 +1,24 @@
+        /*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package myhayday;
+
+/**
+ *
+ * @author aiman
+ */
+public class Trigo {
+    //
+    // Atributos
+    //
+    public static final String ICONO = "/img/trigo.png";
+    public static final int DURACION = 2;
+    public static final int EXPERIENCIA = 1;
+    public static final int RIEGO = 10;
+    
+    private String icono;
+    private int duracionProduccion;
+    private int precioVenta;
+    private int riego;
+}
